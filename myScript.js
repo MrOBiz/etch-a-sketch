9d12bba7ffee3d45a8072645b12e2150befe76be
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-06 09:05:55 +0200

    Refactored code for col and row div, added styling

--- myScript.js
+++ myScript.js
@@ -1,15 +1,22 @@
 
-let divNum = [];
+let divRow = [];
+let divCol = [];
 let totalDivs = 16;
 
 const container = document.querySelector(".container");
 
+for(let i = 0; i < totalDivs; i++ ){
+    const row = document.createElement("div");
+    row.classList.add("rowElement");
+    divRow.push(row);
 
-for(let i = 0; i < totalDivs; i++){
-    const div = document.createElement("div");
-    divNum.push(div);
-    div.classList.add("gridElement");
-    container.appendChild(div);
-    console.log(div);
+    for(let j = 0; j < totalDivs; j++){
+        const col = document.createElement("div");
+        divCol.push(col);
+        col.classList.add("colElement");
+        divRow[i].appendChild(col);
+        console.log(col);
+    }
+
+    container.appendChild(row);
 }
-
